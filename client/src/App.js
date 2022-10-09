c341176c55ad98a9a61e941d43929b0d0f91f3e4
vikash452 <vikashp0901@gmail.com>
2022-10-10 00:32:04 +0530

    front end work started

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./screens/Home/Home";
+import List from "./screens/List/List";
+import About from "./screens/About/About";
+import User from "./screens/User/User";
+
 function App() {
-  console.log("hi");
-  return <div>Movies</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="about">
+          <Route index={true} element={<About />} />
+          <Route path="list" element={<List />} />
+          <Route path="user" element={<User />} />
+        </Route>
+
+        <Route path="*" element={<div>Error</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
